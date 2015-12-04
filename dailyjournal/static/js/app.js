d8ac160f17224303fc7a50d4cfae6b665d735797
David Yeung <quxbaz@gmail.com>
2015-12-04 04:53:47 -0500

    Refactor.

--- dailyjournal/static/js/app.js
+++ dailyjournal/static/js/app.js
@@ -9,6 +9,11 @@ String.prototype.format = function() {
   return that;
 };
 
+// Calls the moment function with the preferred format.
+function momentf(s) {
+  return moment(s, 'YYYY-MM-DD');
+}
+
 // Inclusive [1, 5] => 1, 2, 3, 4, 5
 Handlebars.registerHelper('range', function(from, to, block) {
   var accum = '';
@@ -68,7 +73,18 @@ var Year = Backbone.Collection.extend({
     if (typeof year != 'string')
       throw 'Argument must be a string.'
     this.year = year;
-  }
+  },
+
+  // comparator: function(a, b) {
+  //   var x = momentf(a.get('date'));
+  //   var y = momentf(b.get('date'));
+  //   if (x.isBefore(y))
+  //     return -1;
+  //   else if (x.isAfter(y))
+  //     return 1;
+  //   else
+  //     return 0;
+  // }
 
 });
 
@@ -81,6 +97,11 @@ var YearView = Backbone.View.extend({
     'click .day'     : 'editEntry',
     'mouseout .day'  : 'clearDateLabel',
     'mouseover .day' : 'updateDateLabel'
+  },
+
+  initialize: function(options) {
+    this.collection = options.collection;
+    this.year = this.collection.year;
   },
 
   editEntry: function(event) {
@@ -98,7 +119,7 @@ var YearView = Backbone.View.extend({
   clearDateLabel: function(event) {
     var $active_day = $('.day.active', this.el);
     if ($active_day.length) {
-      var date = moment($active_day.data('date')).format('MMM D').toLowerCase();
+      var date = momentf($active_day.data('date')).format('MMM D').toLowerCase();
       $('.date-label', this.el).text(date);
     } else
       $('.date-label', this.el).text('');
@@ -106,34 +127,12 @@ var YearView = Backbone.View.extend({
 
   updateDateLabel: function(event) {
     var $el = $(event.currentTarget);
-    // var date = moment($el.data('date')).format('M/D');
-    var date = moment($el.data('date')).format('MMM D').toLowerCase();
+    var date = momentf($el.data('date')).format('MMM D').toLowerCase();
     $('.date-label', this.el).text(date);
   },
 
-  initialize: function(options) {
-    this.collection = options.collection;
-    this.year = this.collection.year;
-  },
-
   render: function() {
-    var days = [];
-    var start = moment('{0}-{1}-{2}'.format(this.year, '01', '01'));
-    var end = moment('{0}-{1}-{2}'.format(parseInt(this.year) + 1, '01', '01'));
-    var i = 1;
-    while (start.isBefore(end)) {
-      var date = start.format('YYYY-MM-DD');
-      var entry = this.collection.findWhere({'date': date}) || new Entry({'date': date});
-
-      if (start.format('D') == '1' && i != 1) {
-        days[days.length-1].is_end_of_month = true;
-      }
-
-      days.push(_.extend(entry.toJSON(), {i: i}));
-      start.add(1, 'days');
-      i++;
-    }
-    this.$el.html(this.template({year: this.year, days: days}));
+    this.$el.html(this.template({year: this.collection.toJSON()}));
     return this;
   }
 
@@ -153,39 +152,88 @@ var EditorView = Backbone.View.extend({
     this.entry = null;
   },
 
+  getText: function() {
+    var $textarea = $('textarea', this.el);
+    if ($textarea.is(':visible'))
+      return $textarea.val().trim();
+    else
+      return '';
+  },
+
+  setText: function(text) {
+    $('textarea', this.$el).val(text);
+  },
+
   open: function(event, entry) {
-    this.$el.removeClass('active');
+    this.close();
     _.delay(_.bind(function() {
       this.entry = entry;
       this.$el.addClass('active');
-      $('textarea', this.$el).val(entry.get('text'));
+      this.setText(entry.get('text'));
+      this.enableAutosave();
     }, this), 240);
   },
 
   close: function(event) {
+    this.save();
     this.$el.removeClass('active');
     this.entry = null;
+    this.disableAutosave();
   },
 
   save: function(event) {
+    if (this.getText() == '')
+      return;
     if (this.entry == null)
       this.entry = new Entry({'date': $('.day.active').data('date')});
-    this.entry.save({'text': $('textarea').val()});
+    this.entry.save({'text': this.getText()});
+  },
+
+  enableAutosave: function() {
+    this.lastText = this.getText();
+    this.autosaveId = setInterval(_.bind(function() {
+      if (this.getText() != this.lastText)
+        this.save();
+    }, this), 3000);
+  },
+
+  disableAutosave: function() {
+    this.lastText = '';
+    clearInterval(this.autosaveId);
   }
 
 });
 
+// App util functions
+
+// Iterates through each day of the year.
+function iterYear(fn, year) {
+  var start = momentf('{0}-{1}-{2}'.format(year, '01', '01'));
+  var end = momentf('{0}-{1}-{2}'.format(parseInt(year) + 1, '01', '01'));
+  var i = 1;
+  while (start.isBefore(end)) {
+    fn(start.format('YYYY-MM-DD'), i);
+    start.add(1, 'days');
+    i++;
+  }
+}
 
 // App runtime
 
 $(document).ready(function() {
 
-  var year = new Year('2015');
-  var year_view = new YearView({collection: year});
-  var editor_view = new EditorView();
+  var currentYear = moment().format('YYYY');
+  var year = new Year(currentYear);
+  var yearView = new YearView({collection: year});
+  var editorView = new EditorView();
 
   year.fetch().then(function(entries) {
-    year_view.render().$el.prependTo('.year-wrapper');
+    // Creating Entry models where no entries currently exist.
+    iterYear(function(date, i) {
+      if (!year.findWhere({'date': date}))
+        year.add(new Entry({'date': date}));
+    }, currentYear);
+    yearView.render().$el.prependTo('.year-wrapper');
   });
 
 });
